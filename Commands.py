fd02033a252681a7d698ba7579a25c66e7d4c0e5
va9iff <va9iff@gmail.com>
2022-03-14 20:06:31 +0400

    initial commit personal package

---
+++ Commands.py
@@ -0,0 +1,136 @@
+import sublime
+import sublime_plugin
+import os
+import subprocess
+import json
+
+
+def top_active_folder(window_command) -> str:
+    folders = window_command.window.folders()
+    active_file = window_command.window.active_view().file_name()
+
+    if not active_file and folders:
+        return folders[0]
+
+    elif active_file and folders:
+        for folder in folders:
+            if folder==os.path.dirname(active_file):
+                return folder
+
+    elif active_file and not folders:
+        return os.path.dirname(active_file)
+
+
+
+
+
+class SmikoCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        print('SMIKKKO')
+        print(os.path.dirname(self.window.active_view().file_name()))
+
+
+
+class OpenCmdFileCommand(sublime_plugin.WindowCommand): # V
+    def run(self):
+        print('start cmd at', self.window.active_view().file_name())
+        os.system('start cmd /K cd '+os.path.dirname(self.window.active_view().file_name()))
+
+
+class OpenCmdTopFolderCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        print('start cmd at', top_active_folder(self))
+        print("ll",top_active_folder(self))
+        os.system('start cmd /K cd '+top_active_folder(self))
+
+# text command version
+# class OpenCmdFileCommand(sublime_plugin.TextCommand):
+    # def run(self, edit):
+        # print('start cmd at', self.view.file_name())
+        # os.system('start cmd /K cd '+os.path.dirname(self.view))
+
+
+
+# requires node and live-server installed I think
+class LiveServerOnItsTopFolderCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        print('Live Server at',top_active_folder(self))
+        os.system('start /min cmd /K live-server '+top_active_folder(self))
+
+
+
+class PersonalOpenTopFolderCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        os.system('explorer '+top_active_folder(self))
+
+class PersonalOpenFolderCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        os.system('explorer '+os.path.dirname(self.window.active_view().file_name()))
+
+# requres node and prettier installed
+# Build doesn't block the app, so I'll use prettier with build
+class PrettierDontSaveCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        cmd_command = "prettier --use-tabs --arrow-parens \"avoid\" --no-semi"
+        cmd_command +=  ' "'+self.view.file_name()+'"'
+        output = os.popen(cmd_command).read()
+        output_string = output.replace('\\n','\n')
+        self.view.erase(edit, sublime.Region(0, self.view.size()))
+        self.view.insert(edit, 0, output_string)
+
+
+# requres node and prettier installed
+class BuildPrettierHideOutputCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.run_command("save", {"async": False})
+        self.window.run_command("build", {"build_system": "Packages/Personal/External Tools/Node Tools/Prettier/Prettier - code format.sublime-build", "choice_build_system": True, "choice_variant": True, "variant": ""})
+        self.window.run_command("hide_panel", {"panel": "output.exec"})
+
+# requres node and prettier installed
+# Build doesn't block the app, so I'll use prettier with build
+# class PrettierFormatCommand(sublime_plugin.WindowCommand):
+    # def run(self):
+        # view = self.window.active_view()
+        # view.run_command('prettier_dont_save')
+        # view.run_command('save')
+
+
+# different way
+# class PrettierDontSaveCommand(sublime_plugin.TextCommand):
+#     def run(self, edit):
+#         output = subprocess.check_output(['prettier',self.view.file_name()], shell=True)
+#         output_string = str(output).replace('\\n','\n')[2:-1]
+
+#         self.view.erase(edit, sublime.Region(0, self.view.size()))
+#         self.view.insert(edit, 0, output_string)
+
+
+
+# C:\Users\Vagif\AppData\Roaming\Sublime Text\Packages\Personal\Todos\setting-and-keybindings\preferences-settings.sublime-settings
+
+class ZetupPersonalPreferencesCommand(sublime_plugin.ApplicationCommand):
+    def run(self):
+        script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+        prefs_path = os.path.join(sublime.packages_path(), 'User', 'Preferences.sublime-settings')
+        personal_prefs_path = os.path.join(sublime.packages_path(), 'Personal', 'Personal Settings', 'Preferences.sublime-settings')
+        prefs           = open(prefs_path,'w')
+        personal_prefs  = open(personal_prefs_path)
+        prefs.write(personal_prefs.read())
+
+        prefs.close()
+        personal_prefs.close()
+
+class ZetupPersonalKeybindingCommand(sublime_plugin.ApplicationCommand):
+    def run(self):
+        script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+        prefs_path = os.path.join(sublime.packages_path(), 'User', 'Default (Windows).sublime-keymap')
+        personal_prefs_path = os.path.join(sublime.packages_path(), 'Personal', 'Personal Settings', 'Default (Windows).sublime-keymap')
+        prefs           = open(prefs_path,'w')
+        personal_prefs  = open(personal_prefs_path)
+        prefs.write(personal_prefs.read())
+
+        prefs.close()
+        personal_prefs.close()
+
+
+
