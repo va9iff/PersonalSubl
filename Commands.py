33fecf5caef592e133b9eca293a0e8a9dd0072ce
va9iff <va9iff@gmail.com>
2022-03-15 19:11:28 +0400

    fix active folder for nested folders

--- Commands.py
+++ Commands.py
@@ -5,17 +5,22 @@ import subprocess
 import json
 
 
-def top_active_folder(window_command) -> str:
+def top_active_folder(window_command):
     folders = window_command.window.folders()
     active_file = window_command.window.active_view().file_name()
 
-    if not active_file and folders:
+    print(folders)
+    print(active_file)
+
+    if active_file and folders:
+        for folder in folders:
+            # means string of name is in path of folder
+            if folder in active_file:
+                return folder
+
+    elif not active_file and folders:
         return folders[0]
 
-    elif active_file and folders:
-        for folder in folders:
-            if folder==os.path.dirname(active_file):
-                return folder
 
     elif active_file and not folders:
         return os.path.dirname(active_file)
