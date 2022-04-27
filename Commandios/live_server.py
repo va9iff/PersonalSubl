a5aeca4519bec6e12fa9ba5dc40696256a7babfc
va9iff <va9iff@gmail.com>
2022-04-27 19:06:46 +0400

    add file folder live-server command

--- Commandios/live_server.py
+++ Commandios/live_server.py
@@ -7,3 +7,14 @@ class LiveServerOnItsTopFolderCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		self.window.status_message('Live Server at >> '+top_active_folder(self))
 		os.system('start /min cmd /K live-server '+top_active_folder(self))
+
+class LiveServerOnFileFolderCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		ffolder = self.window.active_view().file_name().split('\\')
+		ffolder.pop()
+		ffolder = "\\".join(ffolder)
+		print(ffolder)
+		self.window.status_message('Live Server at >> '+ ffolder)
+		os.system('start /min cmd /K live-server ' + ffolder)
+
+
